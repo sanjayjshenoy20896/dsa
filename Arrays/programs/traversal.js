720cef8693f2f1514b63c08abfdb3a1a55daf6e4
unknown <sanjayjshenoy20896@gmail.com>
2025-11-16 09:41:04 +0530

    array functions

--- Arrays/programs/traversal.js
+++ Arrays/programs/traversal.js
@@ -1,4 +1,15 @@
 //traversal
 console.log("traversal of arrays");
 
-const numbers = []
+const numbers = [1,2,3,4];
+const strings = ["one","two","three","four"];
+console.log("numbers",numbers);
+console.log("string",strings);
+
+for(let i=0;i<numbers.length;i++){
+    console.log("i",numbers[i])
+}
+
+for(let i=0;i<strings.length;i++){
+    console.log("i",strings[i])
+}
