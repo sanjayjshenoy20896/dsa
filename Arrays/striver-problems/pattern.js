c2d018a2b1514dca76f29ea9a35563585cb509fe
unknown <sanjayjshenoy20896@gmail.com>
2025-11-23 20:20:28 +0530

    striver:pattern 11 our of 22 completed

--- Arrays/striver-problems/pattern.js
+++ Arrays/striver-problems/pattern.js
@@ -3,6 +3,7 @@
  */
 
 // make a pattern to uniform pattern with N lines and a starts on the way
+//1
 function makeUniformSquarePattern(lines){
     const string = "*"
     for(let i=0;i<lines;i++){
@@ -12,6 +13,7 @@ function makeUniformSquarePattern(lines){
 //makeUniformSquarePattern(4);
 
 // make a pattern which is a right angled triangle with N lines + numbers; hypotenuse on the lft side;
+//2
 function makeStarRightAngledTriangleWithLeftHypotenuse(lines){
     const string = "*";
     for(let i=0;i<lines;i++){
@@ -20,7 +22,7 @@ function makeStarRightAngledTriangleWithLeftHypotenuse(lines){
 }
 //makeStarRightAngledTriangleWithLeftHypotenuse(9)
 
-
+//3
 function makeNumberedPatternRightAngledTriangleWithLeftHypotenuse(lines){
     for(let i=1;i<=lines;i++){
         let string = ""
@@ -32,6 +34,7 @@ function makeNumberedPatternRightAngledTriangleWithLeftHypotenuse(lines){
 } 
 // makeNumberedPatternRightAngledTriangleWithLeftHypotenuse(10)
 
+//4
 function makeSameNumberesPatternRightAngledTriangleWithLeftHypotenuse(lines){
     
     for(let i=1;i<=lines;i++){
@@ -39,4 +42,118 @@ function makeSameNumberesPatternRightAngledTriangleWithLeftHypotenuse(lines){
         console.log(str.toString().repeat(i))         
     }
 }
-makeSameNumberesPatternRightAngledTriangleWithLeftHypotenuse(9)
+// makeSameNumberesPatternRightAngledTriangleWithLeftHypotenuse(9)
+
+//5
+function makeUpsideDownTriangle(lines){
+    let str = "*"
+    for(let i=lines;i>0;i--){
+        console.log(str.repeat(i))
+    }
+}
+// makeUpsideDownTriangle(5);
+
+function generateStringFromNumber(number){
+    let str = "";
+    for(let i=1;i<=number;i++){
+        str+=i
+    }
+    return str;
+}
+
+//6
+function makeUpsideDownNumberedTriangle(lines){
+    for(let i=lines;i>0;i--){
+        const mystr = generateStringFromNumber(i)
+        console.log(mystr);
+    }
+}
+
+// makeUpsideDownNumberedTriangle(5);
+
+//7
+function makeEquilateralTriangle(lines){
+    for(let i=1;i<=lines;i++){
+        let pattern = "";
+        // Add leading spaces
+        for(let j=1;j<=lines-i;j++){
+            pattern+= " ";
+        }
+        // Add stars and spaces between them
+        for(let k=1;k<=i;k++){
+            pattern+= "* "
+        }
+        console.log(pattern);
+    }
+}
+// makeEquilateralTriangle(9)
+
+
+//8
+function makeUpsideDownEquilateralTriangle(lines){
+    for(let i=lines;i>=0;i--){
+        let pattern = "";
+        for(let k=1;k<=lines-i;k++){
+            pattern+= " "
+        }
+        for(let j=1;j<=i;j++){
+            pattern+= "* "
+        }
+        
+        console.log(pattern);
+    }
+}
+
+// makeUpsideDownEquilateralTriangle(9);
+
+//9
+function combine2EquilateralTriangle(lines){
+    makeEquilateralTriangle(lines);
+    makeUpsideDownEquilateralTriangle(lines);
+}
+// combine2EquilateralTriangle(9)
+
+//10
+function symmetricalEquilateralTriangle(lines){
+    const string = "*"
+    for(let i=1;i<lines;i++){
+        console.log(string.repeat(i))
+    }
+    console.log(string.repeat(lines));
+    for(let j=lines-1;j>=0;j--){
+        console.log(string.repeat(j))
+    }
+}
+
+// symmetricalEquilateralTriangle(6)
+
+//11 incomplete
+function makeRightAngledTriangleWithzerosAndOnes(lines){
+    for(let i=1;i<lines;i++){
+        let string = "";
+        for(let j=i+1;j<lines-1;j++){
+            if(j%2==0){
+                string+="0"
+            }else{
+                string+="1"
+            }
+        }
+        console.log(string)
+    }
+}
+
+
+
+
+//12 -> string reversed with spaces for palindrome sequences;
+function make2RingAnglesTriangleNextToEachOther(lines){
+    const fixedPattern = " ";
+    for(let i=1;i<=lines;i++){
+        const string = generateStringFromNumber(i);
+        const reversedString = string.split("").reverse().join("");
+        console.log(`${string}${fixedPattern.repeat((lines-i)*2)}${reversedString}`)
+    }
+}
+
+// make2RingAnglesTriangleNextToEachOther(4)
+
