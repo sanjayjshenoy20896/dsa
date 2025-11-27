0613df2f7bac0bc39195fcacd0bef123dab8401c
unknown <sanjayjshenoy20896@gmail.com>
2025-11-27 18:18:08 +0530

    striver: strings medium codes part 1

--- Arrays/striver-problems/programs.js
+++ Arrays/striver-problems/programs.js
@@ -229,4 +229,100 @@ function findANumberThatAppearsTwiceAndRestOnce(arr){
   }
   return-1;
 }
-console.log(findANumberThatAppearsTwiceAndRestOnce([1,4,4,2,3,1]))
+// console.log(findANumberThatAppearsTwiceAndRestOnce([1,4,4,2,3,1]))
+
+
+//longest subarray with target sum k(positives); -> revise
+function findLengthOfSubarrayForTargetSum(arr,target){
+  const n = arr.length;
+
+   // To store the maximum length of the subarray
+        let maxLen = 0;
+        
+        // Pointers to mark the start and end of window
+        let left = 0, right = 0;
+        
+        // To store the sum of elements in the window
+        let sum = arr[0];
+        
+        // Traverse all the elements
+        while (right < n) {
+            
+            // If the sum exceeds K, shrink the window
+            while (left <= right && sum > target) {
+                sum -= arr[left];
+                left++;
+            }
+            
+            // Store the maximum length
+            if (sum === target) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+            
+            right++;
+            if (right < n) sum += arr[right];
+        }
+        
+        return maxLen;
+}
+// console.log(findLengthOfSubarrayForTargetSum([10,5,2,7,1,9],15));
+
+// sort an array of 0's , 1's and 2's
+function sortAnArrayWithNumbersTogether(arr){
+  let ones = 0;
+  let twos = 0;
+  let zeros = 0;
+  let others = [];
+  for(let i=0;i<arr.length;i++){
+    if(arr[i] === 0){
+      zeros++
+    }else if(arr[i] == 1){
+      ones++
+    }else if(arr[i] == 2){
+      twos++
+    }else{
+      others.push(arr[i])
+    }
+  }
+  return [].concat(Array(zeros).fill(0)).concat(Array(ones).fill(1)).concat(Array(twos).fill(2)).concat(...others);
+}
+console.log(sortAnArrayWithNumbersTogether([0,1,2,0,0,2,1,1,3,4,0,0,1,2,6]))
+
+// two sum problem
+function twoSum(arr,target){
+    const numMap = {};
+
+    for(let i=0;i<arr.length;i++){
+        const currentNum = arr[i];
+        const complement = target - currentNum;
+        if(numMap[complement] !== undefined){
+            return [numMap[complement],i]
+        }
+        numMap[currentNum] = i;
+    }
+    return numMap; 
+}
+//time complexity O(n) , space complexity O(n)
+const indices = twoSum([2,7,11,15,6,3],9);
+// console.log("indices",indices);
+
+//Majority element - > The majority element of an array is an element that appears more than n/2 times in the array. The array is guaranteed to have a majority element.
+
+
+function findMajorityElement(arr){
+  let n = arr.length;
+  for(let i=0;i<arr.length;i++){
+
+    let count = 0;
+    for(let j = 0;j<arr.length;j++){
+      if(arr[j] === arr[i]){
+        count++;
+      }
+      if(count<Math.floor(n/2)){
+        return arr[i]
+      }
+    }
+  }
+}
+// console.log(findMajorityElement([7, 0, 0, 1, 7, 7, 2, 7, 7]))
+
