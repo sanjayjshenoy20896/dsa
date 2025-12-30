78291478db9c94d85c888c2acfaad684b8c7c831
unknown <sanjayjshenoy20896@gmail.com>
2025-12-30 09:26:00 +0530

    striver string problems

--- String/striver-programs-medium.js
+++ String/striver-programs-medium.js
@@ -51,3 +51,67 @@ The algorithm for myAtoi(string s) is as follows:
 3. Conversion: Read the integer by skipping leading zeros until a non-digit character is encountered or the end of the string is reached. If no digits were read, then the result is 0.
 4. Rounding: If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then round the integer to remain in the range. Specifically, integers less than -231 should be rounded to -231, and integers greater than 231 - 1 should be rounded to 231 - 1.
  */
+
+function convertAtoI(str){
+    let i=0;
+    let sign = 1;
+    let res = 0;
+
+    // Skip leading whitespaces
+    while(i<str.length && str[i] == " ") i++;
+
+    // return 0 if only spaces
+    if(i === str.length) return 0;
+
+    // Check for optional '+' or '-' sign
+    if(str[i] == "-"){
+        sign = -1;
+        i++;
+    }else if(str[i] == "+"){
+        i++;
+    }
+
+    // Convert characters to integer while valid digits
+    while (i < str.length && str[i] >= '0' && str[i] <= '9') {
+        res = res * 10 + (str[i].charCodeAt(0) - '0'.charCodeAt(0));
+
+        // Clamp to 32-bit signed integer range
+        if (sign * res > 2147483647) return 2147483647;
+        if (sign * res < -2147483648) return -2147483648;
+
+        i++;
+    }
+    return sign*res;
+}
+
+// console.log(convertAtoI("1337c0d3"));
+// console.log(convertAtoI("   -42"));
+
+// count number of substrings from a sequence of chars with k distinct chars -> incomplete
+function atMostKDistinctString(s,k){
+    let left = 0;
+    let res = 0;
+    const freq = new Map();
+    // iterate with right pointer for a sliding window
+    for(let right = 0;right<s.length;right++){
+        freq.set(s[right],(freq.get(s[right] || 0) +1));
+
+        // shrink the window if disctinct exceeds k
+        while(freq.size>k){
+            freq.set(s[left],freq.get(s[left]) -1);
+            if(freq.get(s[left]) === 0) freq.delete(s[left]);
+            left++;
+        }
+        res+=(right-left+1)
+
+    }
+    // return result
+    return res;
+}
+
+function countNumberOfSubstrings(s,k){
+    return atMostKDistinctString(s,k) - atMostKDistinctString(s,k-1);
+}
+// countNumberOfSubstrings("pqpqs",2);
+
+
