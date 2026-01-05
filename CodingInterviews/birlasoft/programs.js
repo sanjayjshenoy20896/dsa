d1271706cb27864404b68dcdc4d3ef013faaf1b0
unknown <sanjayjshenoy20896@gmail.com>
2026-01-05 17:24:55 +0530

    batch processing and object mapping

---
+++ CodingInterviews/birlasoft/programs.js
@@ -0,0 +1,81 @@
+// batch processing
+const input = [1,2,3,4,5,6,7,8,9,10];
+function performBatchProcessing(arr,n){
+  // print elements at batches of n
+ 
+  //split the initial arr in batches of n;
+  for (let i = 0; i < arr.length; i += n) {
+    const batch = arr.slice(i, i + n);
+    console.log(batch);
+  }
+    
+}
+// performBatchProcessing(input,1)
+
+// object mapping
+// const input = { S: [28, 29, 30], M: [29, 30, 31] };
+// Output:
+// 	{    
+// 		28: [s],
+//         29:[S,M],
+//         30: [S,M],
+//         31: [M]
+//       }
+
+function performObjectMapping(arr1,arr2){
+  let mappedObj = {};
+  for(let i=0;i<arr1.length;i++){
+    if(!mappedObj[arr1[i]]){
+      mappedObj[arr1[i]] = ["S"]
+    }
+  }
+  for(let j =0;j<arr2.length;j++){
+    if(!mappedObj[arr2[j]]){
+      mappedObj[arr2[j]] = ["M"]
+    }else{
+      mappedObj[arr2[j]].push("M")
+    }
+  }
+  console.log(mappedObj)
+  return mappedObj;
+}
+// let output = performObjectMapping([28,29,30],[29,30,31]);
+
+function performObjectMappingUsingSinglePass(arr1,arr2){
+    const result = {};
+
+  function add(key, value) {
+    if (!result[key]) {
+      result[key] = [];
+    }
+    result[key].push(value);
+  }
+
+  arr1.forEach(item => add(item, "S"));
+  arr2.forEach(item => add(item, "M"));
+
+  return result;
+}
+// let output = performObjectMappingUsingSinglePass([28,29,30],[29,30,31]);
+// console.log(output)
+
+function performObjectMappingUsingMapFunction(arr1,arr2){
+  const map = new Map();
+
+  arr1.forEach(item => {
+    map.set(item, ["S"]);
+  });
+
+  arr2.forEach(item => {
+    if (map.has(item)) {
+      map.get(item).push("M");
+    } else {
+      map.set(item, ["M"]);
+    }
+  });
+
+  return Object.fromEntries(map);
+}
+// console.log("performObjectMappingUsingMapFunction")
+// const output  = performObjectMappingUsingMapFunction([28,29,30],[29,30,31])
+// console.log(output)
