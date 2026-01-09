860551058a0a07d410b3cbd199e4b65e631b5d55
unknown <sanjayjshenoy20896@gmail.com>
2026-01-09 13:41:31 +0530

    find kth smallest element in an array

--- Arrays/striver-problems/programs.js
+++ Arrays/striver-problems/programs.js
@@ -875,6 +875,7 @@ console.log(countLengthOfSubArrayWithTargetSum([1,2,3],3))
 
 
 // find the kth largest element
+// time complexity: O(n logn) space complexity: O(N)
 function findKthLargestElementBySortingTheArray(arr,k){
   arr.sort((a,b)=> b-a);
   return arr[k - 1];
@@ -893,8 +894,8 @@ function findKthLargestElementBySortingTheArrayAndRemovingDuplicates(arr,k){
 console.log(findKthLargestElementBySortingTheArrayAndRemovingDuplicates([1,15,17,9,12,2,12],2));
 
 // find the kth largest element by using the priority queue
+// time complexity -> O(N * LOG K) space complexity -> O(K)
 function findKthLargestElementByUsingPriorityQueue(arr,k){
-
    // Min heap to store K largest elements
   let priorityQueue = [];
   arr.forEach((number)=>{
@@ -907,3 +908,35 @@ function findKthLargestElementByUsingPriorityQueue(arr,k){
   return priorityQueue[0];
 }
 console.log(findKthLargestElementByUsingPriorityQueue([1,15,17,9,12,2,2,2,2],2))
+
+
+// finding the kth smallest element by sorting the array
+function findKthSmallestElementBySortingArray(arr,k){
+  arr.sort((a,b)=> a-b);
+  return arr[k-1]
+}
+console.log(findKthSmallestElementBySortingArray([1,15,17,9,12,2],2))
+
+// finding the kth smallest element by sorting the array and removing duplicates
+function findKthSmallestElementBySortingArrayAndRemovingDuplicates(arr,k){
+  let inputArr = [...new Set(arr.sort((a,b)=> a-b))];
+  return inputArr[k-1];
+}
+console.log(findKthSmallestElementBySortingArrayAndRemovingDuplicates([1,15,17,9,12,2,2],2))
+
+
+// find the kth smalles element by using priority queue
+function findKthSmallestElementByUsingPriorityQueue(arr,k){
+  let pq = [];
+  arr.forEach((number)=>{
+    pq.push(number);
+    pq.sort((a,b)=>b-a);
+    if(pq.length >k){
+      pq.shift()
+    }
+  })
+  return pq[0];  
+}
+
+
+console.log(findKthSmallestElementByUsingPriorityQueue([1,15,17,9,12,2],2))
