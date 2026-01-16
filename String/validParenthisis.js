0b22e95d7a3569cd563660be1e0cad451857a42e
unknown <sanjayjshenoy20896@gmail.com>
2026-01-16 15:58:20 +0530

    string: valid parentheiss

---
+++ String/validParenthisis.js
@@ -0,0 +1,22 @@
+// valid parentheis
+function validParentheisUsingStack(str){
+    const stack = [];
+    const bracketPairs = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
+    for(const s of str){
+        if(bracketPairs[s]){
+            stack.push(bracketPairs[s])
+        }else{
+            if(stack.pop() !== s){
+                return false;
+            }
+        }
+    }
+    return stack.length ===0
+}
+console.log(validParentheisUsingStack("{}"));
+console.log(validParentheisUsingStack("{}[]()"));
+console.log(validParentheisUsingStack("[}"));
